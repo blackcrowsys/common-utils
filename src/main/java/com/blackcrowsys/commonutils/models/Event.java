8a6ed86333a03c0874ba5fe07fce46f7c0c03d17
ramindurs <ramindur.singh@blackcrowsys.com>
2018-06-07 13:11:29 +0100

    Default array list for event

--- src/main/java/com/blackcrowsys/commonutils/models/Event.java
+++ src/main/java/com/blackcrowsys/commonutils/models/Event.java
@@ -3,11 +3,12 @@ package com.blackcrowsys.commonutils.models;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Event implements Serializable {
 
     private String securityToken;
-    private List<FieldChange> changes;
+    private List<FieldChange> changes = new ArrayList<>();
 }
